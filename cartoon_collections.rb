7eb0f3a75a72fd29da4820827173ffe171f24ee2
Charlie Russo <cparusso@gmail.com>
2018-07-31 21:56:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  ingredients[ingredients.includes?(cheese_types)]
+  ingredients[ingredients.include?(cheese_types)]
 end
